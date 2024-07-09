2d9af4e598aad22b3c9a0b7ae82fe933a6b5cc95
andre.zambroni <contato.andrezambroni@gmail.com>
2024-07-09 18:43:08 -0300

    equals

---
+++ src/main/java/br/com/az/ComparacaoAPP.java
@@ -0,0 +1,13 @@
+package br.com.az;
+
+public class ComparacaoAPP {
+
+    public static void main(String[] args) {
+        String string = "andre";
+        String string1 = new String("andre");
+
+        System.out.println(string1.equalsIgnoreCase(string));
+    }
+
+
+}
